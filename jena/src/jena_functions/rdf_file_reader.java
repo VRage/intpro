7e41659732dc47652e396a72226a1f257c6ba5ff
Arek <phoenix1986@gmx.net>
2014-05-24 14:56:52 +0200

    rdf_file_reader modified and optimized so it must not be innitialized to read a file.

--- jena/src/jena_functions/rdf_file_reader.java
+++ jena/src/jena_functions/rdf_file_reader.java
@@ -31,6 +31,7 @@ import org.apache.jena.riot.RiotException;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.ModelFactoryBase;
 import com.hp.hpl.jena.util.FileManager;
 
 public class rdf_file_reader{
@@ -38,23 +39,11 @@ public class rdf_file_reader{
 	
 	
 	//file-path related strings
-	private String path;
-	private String filename;
-	private final String ROOT_PATH;
-	
-	//attributes
-	private Model model;
-	private InputStream in;
-	
-	//default-constructor
-	public rdf_file_reader(){
-		this.ROOT_PATH = "./";
-		this.model     = ModelFactory.createDefaultModel();
-	}//end constructor rdf_file_reader()
+	private static String ROOT_PATH = "./";
 	
 	/*********************************************************************
 	 * read_file(filename, path)
-	 * opens an existing RDF-file and reads the content into a rdf-model object
+	 * opens an existing RDF-file and reads the content into a rdf-model object + returning this model to caller
 	 * @param filename > string with the name of the rdf-file to open
 	 * @param path     > string with the absolute or relative path of the rdf-file
 	 * 				   > relative paths must be in the same root directory 
@@ -62,11 +51,12 @@ public class rdf_file_reader{
 	 *                   for the root-directory is "./" followed by subfolders
 	 *                   "./subfolderexample/subsubfolderexample/"
 	 **********************************************************************/
-	public void read_file(String filename, String path) throws IllegalArgumentException, RiotException {
-		this.path     = path;
-		this.filename = filename;
+	public static Model read_file(String filename, String path) throws IllegalArgumentException, RiotException {
+		//creating file path with filename string
 		String filepath = path + filename;
-		this.in       = FileManager.get().open(filepath);
+		
+		//creating Inputstream to valid file existence
+		InputStream in  = FileManager.get().open(filepath);
 		
 		if(in == null){
 			throw new IllegalArgumentException("File not found in:\n" 
@@ -74,23 +64,29 @@ public class rdf_file_reader{
 												+"Filename: " + filename);
 		}//end if(in == null)
 		
-		model.read((this.path + this.filename) , null);
+		//creating and returning the model to the caller
+		return ModelFactory.createDefaultModel().read((filepath) , null);
 	}//end method open_file(filename, path)
 	
 	
 	/*********************************************************************
 	 * read_file(filename)
-	 * opens an existing RDF-file which is placed in the root-folder
+	 * opens an existing RDF-file which is placed in the root-folder or an default-safe-path
 	 * and reads the content into a rdf-model object
 	 * @param filename > string with the name of the rdf-file to open
 	 **********************************************************************/
-	public void read_file(String filename) throws IllegalArgumentException, RiotException{
-		read_file(filename, ROOT_PATH);
+	public static Model read_file(String filename) throws IllegalArgumentException, RiotException{
+		return read_file(filename, ROOT_PATH);
 	}//end method open_file(filename)
 	
-	
-	public Model get_RDFModel(){
-		return this.model;
-	}//end method getRDFModel()
-	
+	/*********************************************************************
+	 * set_ROOT_PATH(path)
+	 * sets the ROOT_PATH attribute to the given path
+	 * for example to set a default read and safe directory for the RDF Files
+	 * the default ROOT_PATH is the project root-directory "./"
+	 * @param path - the new default path to the rdf files.
+	 *********************************************************************/
+	public static void set_ROOT_PATH(String path){
+		ROOT_PATH = path;
+	}
 }
