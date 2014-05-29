29f5a369e210057fd454e66d82d94dfa0287713a
Arek <phoenix1986@gmx.net>
2014-05-29 19:02:06 +0200

    demo model without exception handlings implemented

--- jena/src/demo_jena_functions/demo_Model.java
+++ jena/src/demo_jena_functions/demo_Model.java
@@ -1,13 +1,24 @@
 package demo_jena_functions;
 
+import java.io.FileNotFoundException;
 import java.util.Observable;
 
+import org.apache.http.conn.HttpHostConnectException;
+
+import jena_functions.ExportFromFuseki;
+import jena_functions.ImportToFuseki;
+import jena_functions.fuseki_connector;
 import jena_functions.rdf_file_reader;
 
+import com.hp.hpl.jena.query.DatasetAccessor;
 import com.hp.hpl.jena.rdf.model.Model;
 
 import exceptions.RDFFileExceptions.RDFFileNotFoundException;
 import exceptions.RDFFileExceptions.RDFFileNotValidException;
+import exceptions.fuseki_exceptions.NoDatasetAccessorException;
+import exceptions.fuseki_exceptions.NoDatasetGraphException;
+import exceptions.fuseki_exceptions.NoPagesDirException;
+import exceptions.fuseki_exceptions.NoServerConfigException;
 
 /**************************************************************
  * demo_Model
@@ -23,12 +34,54 @@ import exceptions.RDFFileExceptions.RDFFileNotValidException;
 public class demo_Model extends Observable{
 	
 	Model RDFModel;
+	fuseki_connector server;
+	ImportToFuseki imp;
+	ExportFromFuseki exp;
+	
+	public demo_Model(){
+		try {
+			server = new fuseki_connector("/ds", "./fuseki_api/pages", "./fuseki_api/log4j.properties");
+		} catch (NoPagesDirException e) {
+			/***********************************
+			 * HIER MUSS NOCH NE FEHLERBEHANLDUNG REIN!
+			 ************************************/
+			e.printStackTrace();
+		}//end try/catch
+		
+		imp = new ImportToFuseki(getAccessor(), RDFModel);
+		exp = new ExportFromFuseki(getAccessor());
+		
+		
+	}//end constructor
+	
+	
+	/*********************************************************************
+	 * startServer()
+	 * starts the demo fuseki server
+	 **********************************************************************/
+	public void startServer(){
+		try {
+			server.start();
+		} catch (NoDatasetGraphException e) {
+			/***********************************
+			 * HIER MUSS NOCH NE FEHLERBEHANLDUNG REIN!
+			 ************************************/
+			e.printStackTrace();
+		} catch (NoServerConfigException e) {
+			/***********************************
+			 * HIER MUSS NOCH NE FEHLERBEHANLDUNG REIN!
+			 ************************************/
+			e.printStackTrace();
+		}
+	}//end startServer()
+	
+	
 	
 	
 	/*********************************************************************
 	 * readRDFFile(filename, path)
 	 * calls the read_file function of the class rdf_file_reader and stores the return value
-	 * in the RDFModel
+	 * in the RDFModel + imports the file to the connected server
 	 * @param filepath >the path to the rdf file
 	 * 				   > relative paths must be in the same root directory 
 	 *                   as the rdf_file_reader class. the representative shortcut 
@@ -37,7 +90,51 @@ public class demo_Model extends Observable{
 	 **********************************************************************/
 	public void readRDFFile(String filepath) throws RDFFileNotFoundException, RDFFileNotValidException{
 		RDFModel = rdf_file_reader.read_file(filepath);
+		imp.setRdfdata(RDFModel);
+		imp.setAccesor(getAccessor());
+		try {
+			imp.storeToFuseki();
+		} catch (HttpHostConnectException e) {
+			/***********************************
+			 * HIER MUSS NOCH NE FEHLERBEHANLDUNG REIN!
+			 ************************************/
+			e.printStackTrace();
+		}
+		
 	}//end readRDFFile(filename, path)
+	
+	/*********************************************************************
+	 * safeRDFFile(filePath, filename, filetype)
+	 * safes the current rdf model from the fuseki server into a given file with
+	 * a given scheme (turtle, owl etc.)
+	 * @param filePath > the folder where the file shall be safed
+	 * @param filename > the filename of the new to be created file
+	 * @param filytype > the name of the ontologie of the outputfile
+	 **********************************************************************/
+	public void safeRDFFile(String filePath, String filename, String filetype){
+		exp.setFilepath(filePath);
+		try {
+			exp.writeTo(filename, filetype);
+		} catch (HttpHostConnectException e) {
+			/***********************************
+			 * HIER MUSS NOCH NE FEHLERBEHANLDUNG REIN!
+			 ************************************/
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			/***********************************
+			 * HIER MUSS NOCH NE FEHLERBEHANLDUNG REIN!
+			 ************************************/
+			e.printStackTrace();
+		}
+	}//end safeRDFFile(savePath)
+	
+	/*********************************************************************
+	 * closeServer()
+	 * shut down the demo server
+	 **********************************************************************/
+	public void closeServer(){
+		server.stop();
+	}
 	
 	
 	//######################################################################
@@ -46,5 +143,16 @@ public class demo_Model extends Observable{
 		return this.RDFModel;
 	}//end getRDFModel
 	//######################################################################
+	private DatasetAccessor getAccessor(){
+		try {
+			return server.getAccessor();
+		} catch (NoDatasetAccessorException e) {
+			/***********************************
+			 * HIER MUSS NOCH NE FEHLERBEHANLDUNG REIN!
+			 ************************************/
+			e.printStackTrace();
+		}
+		return null;
+	}
 	
 }//end class
